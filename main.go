040ddb4edf7d8fc3d9d69908cf2e897fc1968625
Kuba Smolaga <smolagakuba@gmail.com>
2023-01-22 00:13:40 +0100

    dupa

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/SmartCrops/backend/mqtt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/cors"
 )
 
 const (
@@ -15,6 +19,28 @@ const (
 	waterTopic = "command/69"
 )
 
+type Meas struct {
+	Temp float32
+	Time string
+}
+
+func getData(db *sql.DB) ([]Meas, error) {
+	results, err := db.Query("SELECT Temperature, ts FROM Measurements ORDER BY ts DESC LIMIT 10")
+	if err != nil {
+		return nil, err
+	}
+	var measurements []Meas
+	for results.Next() {
+		var meas Meas
+		err = results.Scan(&meas.Temp, &meas.Time)
+		if err != nil {
+			return nil, err
+		}
+		measurements = append([]Meas{meas}, measurements...)
+	}
+	return measurements, nil
+}
+
 func run() error {
 	mqttClient, err := mqtt.Connect(brokerURL, username, password)
 	if err != nil {
@@ -22,7 +48,25 @@ func run() error {
 	}
 	log.Println("Connected to the broker!")
 
-	http.HandleFunc("/water", func(w http.ResponseWriter, r *http.Request) {
+	// Create the database handle, confirm driver is present
+	db, _ := sql.Open("mysql", "root:smartcrops@/PBL5_meas")
+	defer db.Close()
+
+	// Connect and check the server version
+	var version string
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	fmt.Println("Connected to:", version)
+
+	// var measurements []float32
+	// err = db.QueryRow("SELECT Temperature FROM Measurements").Scan(&measurements)
+	// if err != nil {
+	// 	return fmt.Errorf("failed to query the database: %w", err)
+	// }
+	// log.Println("Got some measurements", len(measurements))
+	// log.Println(measurements)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/water", func(w http.ResponseWriter, r *http.Request) {
 		payload := `{"pumpGpio":0, "durationS":5}`
 		err = mqttClient.Pub(waterTopic, 1, false, payload)
 		if err != nil {
@@ -30,7 +74,16 @@ func run() error {
 		}
 		_, _ = w.Write([]byte("Ok"))
 	})
-	return http.ListenAndServe(":8080", nil)
+	mux.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		measurements, err := getData(db)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+		b, _ := json.Marshal(measurements)
+		w.Write(b)
+	})
+	handler := cors.Default().Handler(mux)
+	return http.ListenAndServe(":8080", handler)
 }
 
 func main() {
